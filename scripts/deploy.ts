c30a3dd0fe316256f99285fc60f390db1bc76e1b
RyoheiKudo <ryo939190@gmail.com>
2022-09-27 01:38:55 +0900

    Update some files to add frontend

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,42 +1,58 @@
-import { ethers } from "hardhat";
+import { artifacts, ethers, network } from "hardhat";
+import path from "path";
+import { Token } from "./../typechain-types/Token";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
 
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
-
-/* 
-// These functions are for Token.
-async function main() {
   const [deployer] = await ethers.getSigners();
-
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
+  await token.deployed();
 
   console.log("Token address:", token.address);
+
+  saveFrontendFiles(token);
+}
+
+function saveFrontendFiles(token: Token) {
+  const fs = require("fs");
+  const contractsDir = path.join(
+    __dirname,
+    "..",
+    "frontend",
+    "src",
+    "contracts"
+  );
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify({ Token: token.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("Token");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Token.json"),
+    JSON.stringify(TokenArtifact, null, 2)
+  );
 }
 
 main()
@@ -44,4 +60,4 @@ main()
   .catch((error) => {
     console.error(error);
     process.exit(1);
-  }); */
+  });
